9f0767bca3c5f31dbe02d86ee276304823a23a6a
ljp2 <ljp2@lehigh.edu>
2024-02-09 07:39:40 -0500

    working

--- t.py
+++ t.py
@@ -1,28 +1,25 @@
-import sys
-from PyQt6.QtWidgets import QApplication
 from PyQt6.QtGui import QPainter, QPicture
-from PyQt6.QtCore import QRectF, QPointF
+from PyQt6.QtCore import QPointF, QRectF
+from PyQt6.QtWidgets import QApplication
 import pyqtgraph as pg
-import numpy as np
 
 class CandlestickItem(pg.GraphicsObject):
     def __init__(self, data):
         pg.GraphicsObject.__init__(self)
-        self.data = data
+        self.data = data  # data must be list of tuples (time, open, high, low, close)
         self.generatePicture()
 
     def generatePicture(self):
         self.picture = QPicture()
         p = QPainter(self.picture)
-        w = (self.data[1][0] - self.data[0][0]) / 3.
-        for (t, open, close, min, max) in self.data:
-            p.setPen(pg.mkPen('w'))
-            p.drawLine(QPointF(t, min), QPointF(t, max))
+        # draw candlesticks
+        for (t, open, high, low, close) in self.data:
+            p.drawLine(QPointF(t, high), QPointF(t, low))
             if open > close:
                 p.setBrush(pg.mkBrush('r'))
             else:
                 p.setBrush(pg.mkBrush('g'))
-            p.drawRect(QRectF(t-w, open, w*2, close-open))
+            p.drawRect(QRectF(t-0.3, open, 0.6, close-open))
         p.end()
 
     def paint(self, p, *args):
@@ -31,20 +28,23 @@ class CandlestickItem(pg.GraphicsObject):
     def boundingRect(self):
         return QRectF(self.picture.boundingRect())
 
-app = QApplication(sys.argv)
-data = [  ## fields are (time, open, close, min, max).
-    (1., 10, 13, 5, 15),
-    (2., 13, 17, 9, 20),
-    (3., 17, 14, 11, 23),
-    (4., 14, 15, 5, 19),
-    (5., 15, 9, 8, 22),
-    (6., 9, 15, 8, 16),
-]
-item = CandlestickItem(data)
-plt = pg.plot()
-plt.addItem(item)
-plt.setXRange(1, 6)
-plt.setYRange(4, 24)
+    def update(self, new_candle):
+        self.data.append(new_candle)
+        self.generatePicture()
 
-if __name__ == '__main__':
-    sys.exit(app.exec())
+# Example usage:
+app = QApplication([])
+
+data = [ (1, 10, 15, 5, 13), (2, 13, 20, 12, 15), (3, 15, 22, 10, 20) ]
+candlestick = CandlestickItem(data)
+
+win = pg.GraphicsLayoutWidget()
+plt = win.addPlot(title="Candlestick chart")
+plt.addItem(candlestick)
+
+# Add a new candle
+candlestick.update((4, 20, 23, 15, 22))
+
+win.show()
+
+app.exec()
