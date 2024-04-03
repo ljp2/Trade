bb69b3d548a9b4cff4c512bd8ddf04c364e1b58c
ljp2 <ljp2@lehigh.edu>
2024-04-03 12:51:11 -0400

    Update toUTC.py

--- IB/Historical/toUTC.py
+++ IB/Historical/toUTC.py
@@ -1,13 +1,16 @@
-import pytz
 from datetime import datetime
+import pytz
 
-def us_eastern_to_utc(us_eastern_time):
-    us_eastern = pytz.timezone('US/Eastern')
-    is_dst = us_eastern_time.dst()
-    us_eastern_time = us_eastern.localize(us_eastern_time, is_dst=is_dst)
-    utc_time = us_eastern_time.astimezone(pytz.utc)
-    return utc_time
+def convert_to_utc(year, month, day, hour, minute):
+    nyc_tz = pytz.timezone('America/New_York')
+    utc_tz = pytz.timezone('UTC')
+    nyc_time = nyc_tz.localize(datetime(year, month, day, hour, minute))
+    utc_time = nyc_time.astimezone(utc_tz)
+    return int(utc_time.timestamp())
 
-eastern_time = datetime(2024, 4, 2, 9,30, 0)  # Assuming it's in Eastern Daylight Time
-utc_time = us_eastern_to_utc(eastern_time)
-print("UTC Time:", utc_time)
+def convert_to_nyc(utc_timestamp):
+    utc_tz = pytz.timezone('UTC')
+    nyc_tz = pytz.timezone('America/New_York')
+    utc_time = datetime.fromtimestamp(utc_timestamp, tz=utc_tz)
+    nyc_time = utc_time.astimezone(nyc_tz)
+    return nyc_time
